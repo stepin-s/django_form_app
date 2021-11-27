0822e07eb8df35a08785210a26aa9ed588953a4e
unknown <stpn.s@yandex.ru>
2021-11-28 02:08:04 +0300

    latest

--- project/contact_form/models.py
+++ project/contact_form/models.py
@@ -1,8 +1,14 @@
 from django.db import models
-
+from django.core.validators import(
+    EmailValidator,validate_email
+)
 
 class Contact(models.Model):
-    email = models.CharField(max_length=200, default="test@test.ru")
+    email = models.CharField(max_length=200, default="test@test.ru",validators=[
+        EmailValidator(
+            message="Введите корректный email"
+        )
+    ])
     message = models.CharField(max_length=200, default="some_message") 
 
 
